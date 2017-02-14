062caf270d7fb3d1fe83a73176d7e1f1e504b8be
Mateo Mustapic <mateo.mustapic@liferay.com>
2017-02-14 11:10:20 +0100

    MATEO-1 adding filtering to all columns

--- mateo-mustapic/assignments/proj4/js/script.js
+++ mateo-mustapic/assignments/proj4/js/script.js
@@ -12,11 +12,23 @@ function search() {
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
+    td2 = tr[i].getElementsByTagName("td")[1];
+    td3 = tr[i].getElementsByTagName("td")[2];
+    td4 = tr[i].getElementsByTagName("td")[3];
 
-    if (td) {
+    if (td||td2||td3||td4) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } 
+      else if (td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else if (td3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else if (td4.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
       else {
         tr[i].style.display = "none";
       }
