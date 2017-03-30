821dd85461d4d9cb8ca7cac0ad25213fc56de6ad
Marko Čikoš <marko.cikos@liferay.com>
2017-03-30 18:16:04 +0200

    KRESO-3 When a string is used more then once, define a var for it

--- kresimir-coko/assignments/perzej/sdk/themes/perzej-theme/docroot/_diffs/js/main.js
+++ kresimir-coko/assignments/perzej/sdk/themes/perzej-theme/docroot/_diffs/js/main.js
@@ -2,17 +2,21 @@ AUI().use(
 	'event',
 	'node',
 	function(A) {
+		var EVENT_FOCUS = 'focus';
+
+		var SELECTOR_FIELD = '.field';
+
 		function toggleActive(event) {
 			var fieldInput = event.currentTarget;
 
 			var fieldLabels = fieldInput.siblings('label');
 
-			fieldLabels.toggleClass('label-active', event.type == 'focus');
+			fieldLabels.toggleClass('label-active', event.type == EVENT_FOCUS);
 		}
 
 		var form = A.one('.fieldset');
 
-		form.delegate('blur', toggleActive, '.field');
-		form.delegate('focus', toggleActive, '.field');
+		form.delegate('blur', toggleActive, SELECTOR_FIELD);
+		form.delegate(EVENT_FOCUS, toggleActive, SELECTOR_FIELD);
 	}
 );
