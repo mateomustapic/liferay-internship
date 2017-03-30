9ab08b58d2a0ef670ad1fa40df39a5dd376ae8a5
Marko Čikoš <marko.cikos@liferay.com>
2017-03-30 17:56:47 +0200

    KRESO-3 Simplify

--- kresimir-coko/assignments/perzej/sdk/themes/perzej-theme/docroot/_diffs/js/main.js
+++ kresimir-coko/assignments/perzej/sdk/themes/perzej-theme/docroot/_diffs/js/main.js
@@ -2,25 +2,17 @@ AUI().use(
 	'node',
 	'event',
 	function(A) {
-		function labelFocused(e) {
-			var fieldInput = e.currentTarget;
+		function toggleActive(event) {
+			var fieldInput = event.currentTarget;
 
 			var fieldLabels = fieldInput.siblings('label');
 
-			fieldLabels.addClass('label-active');
-		}
-
-		function labelBlurred(e) {
-			var fieldInput = e.currentTarget;
-
-			var fieldLabels = fieldInput.siblings('label');
-
-			fieldLabels.removeClass('label-active');
+			fieldLabels.toggleClass('label-active', event.type == 'focus');
 		}
 
 		var form = A.one('.fieldset');
 
-		form.delegate('focus', labelFocused, '.field');
-		form.delegate('blur', labelBlurred, '.field');
+		form.delegate('focus', toggleActive, '.field');
+		form.delegate('blur', toggleActive, '.field');
 	}
 );
