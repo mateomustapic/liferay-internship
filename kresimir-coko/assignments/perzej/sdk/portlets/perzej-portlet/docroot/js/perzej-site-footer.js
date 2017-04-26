4ec2851b6cc3686d11f79cb4ea684be3db11024b
Krešimir Čoko <kresimir.coko@liferay.com>
2017-04-26 14:22:18 +0200

    KRESO-3 Removed extra indentation

--- kresimir-coko/assignments/perzej/sdk/portlets/perzej-portlet/docroot/js/perzej-site-footer.js
+++ kresimir-coko/assignments/perzej/sdk/portlets/perzej-portlet/docroot/js/perzej-site-footer.js
@@ -6,121 +6,121 @@ AUI.add(
 		var EVENT_CLICK = 'click';
 
 		var PerzejSiteFooter = A.Component.create(
-				{
-					AUGMENTS: [Liferay.PortletBase],
+			{
+				AUGMENTS: [Liferay.PortletBase],
 
-					EXTENDS: A.Base,
+				EXTENDS: A.Base,
 
-					NAME: 'perzej-site-footer',
+				NAME: 'perzej-site-footer',
 
-					prototype: {
-						initializer: function(config) {
-							var instance = this;
+				prototype: {
+					initializer: function(config) {
+						var instance = this;
 
-							var officeAddressShort = config.officeAddressShort;
+						var officeAddressShort = config.officeAddressShort;
 
-							instance._headquartersAddressShort = config.headquartersAddressShort;
-							instance._headquartersLat = config.headquartersLat;
-							instance._headquartersLng = config.headquartersLng;
-							instance._officeAddressShort = officeAddressShort;
-							instance._officeLat = config.officeLat;
-							instance._officeLng = config.officeLng;
+						instance._headquartersAddressShort = config.headquartersAddressShort;
+						instance._headquartersLat = config.headquartersLat;
+						instance._headquartersLng = config.headquartersLng;
+						instance._officeAddressShort = officeAddressShort;
+						instance._officeLat = config.officeLat;
+						instance._officeLng = config.officeLng;
 
-							var googleMapsWidget = new Liferay.Portlet.GoogleMaps(
+						var googleMapsWidget = new Liferay.Portlet.GoogleMaps(
+							{
+								googleMapsURL: config.googleMapsURL,
+								languageId: config.languageId,
+								mapAddress: officeAddressShort,
+								namespace: config.namespace,
+								portletId: config.portletId
+							}
+						).render();
+
+						googleMapsWidget.set('mapParams.zoom', 14);
+
+						instance._googleMapsWidget = googleMapsWidget;
+
+						var headquartersLink = A.one('.footer-info .headquarters');
+
+						if (headquartersLink) {
+							headquartersLink.on(EVENT_CLICK, A.bind('_changeAddress', instance));
+						}
+
+						var officeLink = A.one('.footer-info .office');
+
+						if (officeLink) {
+							officeLink.on(EVENT_CLICK, A.bind('_changeAddress', instance));
+						}
+					},
+
+					_changeAddress: function(event) {
+						var instance = this;
+
+						var latitude = instance._officeLat;
+						var longitude = instance._officeLng;
+						var shortAddress = instance._officeAddressShort;
+
+						if (event.currentTarget.hasClass('headquarters')) {
+							latitude = instance._headquartersLat;
+							longitude = instance._headquartersLng;
+							shortAddress = instance._headquartersAddressShort;
+						}
+
+						var googleMaps = google.maps;
+
+						var infoWindow = new googleMaps.InfoWindow();
+
+						var googleMapsWidget = instance._googleMapsWidget;
+
+						googleMapsWidget._infoWindow = infoWindow;
+
+						if (!infoWindow) {
+							infoWindow = new googleMaps.InfoWindow(
 								{
-									googleMapsURL: config.googleMapsURL,
-									languageId: config.languageId,
-									mapAddress: officeAddressShort,
-									namespace: config.namespace,
-									portletId: config.portletId
-								}
-							).render();
-
-							googleMapsWidget.set('mapParams.zoom', 14);
-
-							instance._googleMapsWidget = googleMapsWidget;
-
-							var headquartersLink = A.one('.footer-info .headquarters');
-
-							if (headquartersLink) {
-								headquartersLink.on(EVENT_CLICK, A.bind('_changeAddress', instance));
-							}
-
-							var officeLink = A.one('.footer-info .office');
-
-							if (officeLink) {
-								officeLink.on(EVENT_CLICK, A.bind('_changeAddress', instance));
-							}
-						},
-
-						_changeAddress: function(event) {
-							var instance = this;
-
-							var latitude = instance._officeLat;
-							var longitude = instance._officeLng;
-							var shortAddress = instance._officeAddressShort;
-
-							if (event.currentTarget.hasClass('headquarters')) {
-								latitude = instance._headquartersLat;
-								longitude = instance._headquartersLng;
-								shortAddress = instance._headquartersAddressShort;
-							}
-
-							var googleMaps = google.maps;
-
-							var infoWindow = new googleMaps.InfoWindow();
-
-							var googleMapsWidget = instance._googleMapsWidget;
-
-							googleMapsWidget._infoWindow = infoWindow;
-
-							if (!infoWindow) {
-								infoWindow = new googleMaps.InfoWindow(
-									{
-										content: shortAddress
-									}
-								);
-							}
-							else {
-								infoWindow.setContent(shortAddress);
-							}
-
-							var marker = googleMapsWidget._marker;
-
-							if (marker) {
-								marker.setMap(null);
-							}
-
-							var map = googleMapsWidget._map;
-
-							marker = new googleMaps.Marker(
-								{
-									map: map,
-									position: new googleMaps.LatLng(latitude, longitude),
-									title: shortAddress
+									content: shortAddress
 								}
 							);
-
-							infoWindow.open(map, marker);
-
-							googleMapsWidget._marker = marker;
-
-							instance._replaceInfoWindowContent();
-
-							map.panTo(marker.getPosition());
-						},
-
-						_replaceInfoWindowContent: function() {
-							var instance = this;
-
-							var infoWindow = instance._googleMapsWidget._infoWindow;
-
-							var infoWindowContent = infoWindow.getContent();
-
-							infoWindow.setContent(infoWindowContent);
 						}
+						else {
+							infoWindow.setContent(shortAddress);
+						}
+
+						var marker = googleMapsWidget._marker;
+
+						if (marker) {
+							marker.setMap(null);
+						}
+
+						var map = googleMapsWidget._map;
+
+						marker = new googleMaps.Marker(
+							{
+								map: map,
+								position: new googleMaps.LatLng(latitude, longitude),
+								title: shortAddress
+							}
+						);
+
+						infoWindow.open(map, marker);
+
+						googleMapsWidget._marker = marker;
+
+						instance._replaceInfoWindowContent();
+
+						map.panTo(marker.getPosition());
+					},
+
+					_replaceInfoWindowContent: function() {
+						var instance = this;
+
+						var infoWindow = instance._googleMapsWidget._infoWindow;
+
+						var infoWindowContent = infoWindow.getContent();
+
+						infoWindow.setContent(infoWindowContent);
 					}
 				}
+			}
 		);
 
 		Liferay.Portlet.PerzejSiteFooter = PerzejSiteFooter;
