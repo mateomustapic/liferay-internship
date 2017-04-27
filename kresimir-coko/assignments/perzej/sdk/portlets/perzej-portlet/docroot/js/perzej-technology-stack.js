435e83f7b94cc39fc4e26ffb5914899db1e7fe4e
Marko Čikoš <marko.cikos@liferay.com>
2017-04-27 11:04:29 +0200

    KRESO-3 Standardize naming for .each

--- kresimir-coko/assignments/perzej/sdk/portlets/perzej-portlet/docroot/js/perzej-technology-stack.js
+++ kresimir-coko/assignments/perzej/sdk/portlets/perzej-portlet/docroot/js/perzej-technology-stack.js
@@ -22,16 +22,16 @@ AUI().add(
 								var technologyImages = techStackContainer.all('.technology-image');
 
 								technologyImages.each(
-									function(technologyImageNode) {
-										var technologyImage = technologyImageNode.one('img');
+									function(item) {
+										var technologyImage = item.one('img');
 
 										if (technologyImage) {
 											var imageUrl = technologyImage.getAttribute('src');
 
 											if (imageUrl) {
-												technologyImageNode.setStyle('backgroundImage', 'url(' + imageUrl + ')');
+												item.setStyle('backgroundImage', 'url(' + imageUrl + ')');
 
-												technologyImageNode.addClass('compat-object-fit');
+												item.addClass('compat-object-fit');
 											}
 										}
 									}
