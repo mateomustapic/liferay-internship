09da748690bb48bf08afcac3e01db345998dd01f
Krešimir Čoko <kresimir.coko@liferay.com>
2017-05-24 10:29:06 +0200

    CLDSVCS-2709 animateValue function

--- team-assignments/monitor/monitor-singular-web/src/main/resources/META-INF/resources/View.es.js
+++ team-assignments/monitor/monitor-singular-web/src/main/resources/META-INF/resources/View.es.js
@@ -3,7 +3,49 @@ import Soy from 'metal-soy/src/Soy';
 import templates from './View.soy';
 
 class View extends Component {
+	animateValue(valueContainerDOM, startingValue, finalValue, animationDuration) {
+		const valueContainer = document.querySelector(valueContainerDOM);
+
+		const valueRange = finalValue - startingValue;
+
+		const minAnimationDuration = 50;
+
+		let animationStepTime = Math.abs(Math.floor(animationDuration / valueRange));
+
+		animationStepTime = Math.max(animationStepTime, minAnimationDuration);
+
+		const animationStartTime = new Date().getTime();
+
+		const animationEndTime = animationStartTime + animationDuration;
+
+		let animationTimer;
+
+		function runAnimation() {
+			const currentTime = new Date().getTime();
+
+			const remainingTime = Math.max((animationEndTime - currentTime) / animationDuration, 0);
+
+			const currentValue = Math.round(finalValue - (remainingTime * valueRange));
+
+			//TODO: Instead of doing it by changing the DOM, change and update the state
+
+			valueContainer.innerHTML = currentValue;
+
+			if (currentValue === finalValue) {
+				clearInterval(animationTimer);
+			}
+		}
+
+		animationTimer = setInterval(runAnimation, animationStepTime);
+
+		runAnimation();
+	}
+
 	setOnlineServerCount() {
+		//TODO: startingValue and finalValue are retrieved from the API
+
+		this.animateValue('.online-server-count', startingValue, finalValue, 200);
+
 		return 147;
 	};
 
