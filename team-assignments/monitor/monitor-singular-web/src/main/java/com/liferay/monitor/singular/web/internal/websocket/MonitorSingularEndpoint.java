11b9b38165579d586bdee843d70c045539f0212c
Krešimir Čoko <kresimir.coko@liferay.com>
2017-06-02 10:29:16 +0200

    CLDSVCS-2729 Endpoint changes to support messages from server

--- team-assignments/monitor/monitor-singular-web/src/main/java/com/liferay/monitor/singular/web/internal/websocket/MonitorSingularEndpoint.java
+++ team-assignments/monitor/monitor-singular-web/src/main/java/com/liferay/monitor/singular/web/internal/websocket/MonitorSingularEndpoint.java
@@ -18,7 +18,6 @@ import java.io.IOException;
 
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
-import javax.websocket.MessageHandler;
 import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
 
@@ -30,30 +29,21 @@ import org.osgi.service.component.annotations.Component;
  */
 @Component(
 	immediate = true,
-	property = {"org.osgi.http.websocket.endpoint.path=/o/echo"},
+	property = {"org.osgi.http.websocket.endpoint.path=/o/singularEndpoint"},
 	service = Endpoint.class
 )
 public class MonitorSingularEndpoint extends Endpoint {
 
 	@Override
 	public void onOpen(final Session session, EndpointConfig endpointConfig) {
-		session.addMessageHandler(
-			new MessageHandler.Whole<String>() {
+		try {
+			RemoteEndpoint.Basic remoteEndpoint =	session.getBasicRemote();
 
-				@Override
-				public void onMessage(String text) {
-					try {
-						RemoteEndpoint.Basic remoteEndpoint =
-							session.getBasicRemote();
-
-						remoteEndpoint.sendText(text);
-					}
-					catch (IOException ioe) {
-						throw new RuntimeException(ioe);
-					}
-				}
-
-			});
+			remoteEndpoint.sendText("Hello from the server");
+		}
+		catch (IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
 	}
 
 }
