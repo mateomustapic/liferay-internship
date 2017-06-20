ace0f9836516b77377bfd5ff66a323072b1dccf1
Matea Pjanić <matea.pjanic@liferay.com>
2017-06-20 09:22:33 +0200

    MATEA-3 Get data from persistance

---
+++ matea-pjanic/assignments/liferay-app/sdk/portlets/music-portlet/docroot/WEB-INF/src/com/liferay/music/portlet/service/impl/BendLocalServiceImpl.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.music.portlet.service.impl;
+
+import com.liferay.music.portlet.model.Album;
+import com.liferay.music.portlet.model.Bend;
+import com.liferay.music.portlet.model.Song;
+import com.liferay.music.portlet.service.AlbumLocalServiceUtil;
+import com.liferay.music.portlet.service.base.BendLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+
+import java.util.List;
+
+/**
+ * The implementation of the bend local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.music.portlet.service.BendLocalService} interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Matea Pjanic
+ * @see com.liferay.music.portlet.service.base.BendLocalServiceBaseImpl
+ * @see com.liferay.music.portlet.service.BendLocalServiceUtil
+ */
+public class BendLocalServiceImpl extends BendLocalServiceBaseImpl {
+
+	/**
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this interface directly. Always use {@link com.liferay.music.portlet.service.BendLocalServiceUtil} to access the bend local service.
+	 */
+	public Bend findBend(String name) throws PortalException, SystemException {
+		Bend bend = bendPersistence.findByName(name);
+
+		List<Album> albums = getBendAlbums(bend.getBendId());
+
+	for (Album album : albums) {
+		List<Song> songs = AlbumLocalServiceUtil.getAlbumSongs(
+				album.getAlbumId());
+
+		album.setSongs(songs);
+	}
+
+	bend.setAlbums(albums);
+
+	return bend;
+}
+
+	public List<Album> getBendAlbums(long bendId)
+		throws PortalException, SystemException {
+
+		return albumPersistence.findByBendId(bendId);
+	}
+
+}
